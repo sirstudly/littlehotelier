54e28a8b43aaddf9b77fd6ec2bf122deca35fc7a
sirstudly <ron1404@techie.com>
2016-02-13 04:48:55 +0000

    added bookings of 5 guests into 6 bed dorms into group bookings report

--- src/main/java/com/macbackpackers/dao/WordPressDAOImpl.java
+++ src/main/java/com/macbackpackers/dao/WordPressDAOImpl.java
@@ -440,7 +440,7 @@ public class WordPressDAOImpl implements WordPressDAO {
         LOGGER.info( "Running report for job id: " + allocationScraperJobId );
         sessionFactory.getCurrentSession()
                 .createSQLQuery( sql.getProperty( "group.bookings" ) )
-                .setParameter( 0, allocationScraperJobId )
+                .setParameter( "jobId", allocationScraperJobId )
                 .executeUpdate();
     }
 
