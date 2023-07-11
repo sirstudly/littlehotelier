3e119a56e0754f2b24727b4321dfa8741a517f79
sirstudly <ron1404@techie.com>
2023-07-11 02:33:00 +0100

    Fix cloudbeds login with google authenticator

--- src/main/java/com/macbackpackers/scrapers/ChromeScraper.java
+++ src/main/java/com/macbackpackers/scrapers/ChromeScraper.java
@@ -243,7 +243,7 @@ public class ChromeScraper {
                 scaCode.sendKeys( otp );
             }
 
-            WebElement rememberDevice = driver.findElement( By.name( "//label[@data-se-for-name='rememberDevice']" ) );
+            WebElement rememberDevice = driver.findElement( By.xpath( "//label[@data-se-for-name='rememberDevice']" ) );
             rememberDevice.click();
 
             nextButton = driver.findElement( By.xpath( "//input[@data-type='save']" ) );
