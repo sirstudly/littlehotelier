4577cef9896bdaa646bd36588d82a2efc5bf0206
sirstudly <ron1404@techie.com>
2024-03-26 13:34:27 +0000

    Fix bed matcher for LSH for new naming scheme

--- src/main/java/com/macbackpackers/scrapers/matchers/LochsideRoomBedMatcher.java
+++ src/main/java/com/macbackpackers/scrapers/matchers/LochsideRoomBedMatcher.java
@@ -16,17 +16,25 @@ public class LochsideRoomBedMatcher implements RoomBedMatcher {
 
     @Override
     public BedAssignment parse( String roomLabel ) {
-        String room = null;
+        String room;
         String bedName = null;
         Pattern p = Pattern.compile( "(.*)\\((.*)\\)$" );
         Matcher m = p.matcher( roomLabel );
-        if ( m.find() == false ) {
-            LOGGER.debug( "Couldn't determine bed name from '" + roomLabel + "'. Is it a private?" );
-            room = roomLabel.trim();
-        }
-        else {
+        if ( m.find() ) {
             room = m.group( 1 ).trim();
             bedName = m.group( 2 ).trim();
+        }
+        else {
+            p = Pattern.compile( "(.*)-(.*)$" );
+            m = p.matcher( roomLabel );
+            if ( m.find() ) {
+                room = m.group( 1 ).trim(); // Capture group 1 includes whitespace
+                bedName = m.group( 2 ).trim();
+            }
+            else {
+                LOGGER.debug( "Couldn't determine bed name from '" + roomLabel + "'. Is it a private?" );
+                room = roomLabel.trim();
+            }
         }
         return new BedAssignment( room, bedName );
     }
