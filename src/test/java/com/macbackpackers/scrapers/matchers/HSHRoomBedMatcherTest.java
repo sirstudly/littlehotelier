e3b6333788e923f6591df89b0435b5fe1e9e5d0e
sirstudly <ron1404@techie.com>
2026-04-08 21:34:29 +0100

    Updating HSHRoomBedMatcherTest

--- src/test/java/com/macbackpackers/scrapers/matchers/HSHRoomBedMatcherTest.java
+++ src/test/java/com/macbackpackers/scrapers/matchers/HSHRoomBedMatcherTest.java
@@ -7,27 +7,26 @@ public class HSHRoomBedMatcherTest {
 
     @Test
     public void testRoomBedMatcher() throws Exception {
-        assertRoomAndBed( "601AmazonT", "6", "01AmazonT" );
-        assertRoomAndBed( "608HudsonB", "6", "08HudsonB" );
+        assertRoomAndBed( "6P01AmazonT", "6P", "01AmazonT" );
+        assertRoomAndBed( "6P08HudsonB", "6P", "08HudsonB" );
         assertRoomAndBed( "5A01MushroomT", "5A", "01MushroomT" );
-        assertRoomAndBed( "Unallocated", "Unallocated", null );
-        assertRoomAndBed( "LOTR3MerryT", "LOTR", "3MerryT" );
-        assertRoomAndBed( "T3M4D'ArtagnanB", "T3M", "4D'ArtagnanB" );
+        assertRoomAndBed( "7LOTR3MerryT", "LOTR", "3MerryT" );
+        assertRoomAndBed( "7T3M4D'ArtagnanB", "T3M", "4D'ArtagnanB" );
         assertRoomAndBed( "3E01AntT", "3E", "01AntT" );
         assertRoomAndBed( "5F02BuddyHollyB", "5F", "02BuddyHollyB" );
-        assertRoomAndBed( "TCLady and the Tramp", "TC", "Lady and the Tramp" );
+        assertRoomAndBed( "7Lady and the Tramp", "TC", "Lady and the Tramp" );
         assertRoomAndBed( "3D03EelT", "3D", "03EelT" );
-        assertRoomAndBed( "Zoo01AnteaterT", "Zoo", "01AnteaterT" );
-        assertRoomAndBed( "F&V02BroccoliB", "F&V", "02BroccoliB" );
+        assertRoomAndBed( "4Zoo01AnteaterT", "Zoo", "01AnteaterT" );
+        assertRoomAndBed( "4Zoo02BearB", "Zoo", "02BearB" );
+        assertRoomAndBed( "4F&V02BroccoliB", "F&V", "02BroccoliB" );
         assertRoomAndBed( "5G04VenusB", "5G", "04VenusB" );
-        assertRoomAndBed( "TBBatman&Robin", "TB", "Batman&Robin" );
-        assertRoomAndBed( "TAWill&Kate", "TA", "Will&Kate" );
-        assertRoomAndBed( "Unrecognized entry", "Unrecognized entry", null );
+        assertRoomAndBed( "7Batman&Robin", "TB", "Batman&Robin" );
+        assertRoomAndBed( "7Will&Kate", "TA", "Will&Kate" );
     }
 
     private void assertRoomAndBed( String label, String expectedRoom, String expectedBed ) {
         BedAssignment matcher = new HSHRoomBedMatcher().parse( label );
-        Assertions.assertEquals( "room not matched", expectedRoom, matcher.getRoom() );
-        Assertions.assertEquals( "bed not matched", expectedBed, matcher.getBedName() );
+        Assertions.assertEquals( expectedRoom, matcher.getRoom(), "room not matched" );
+        Assertions.assertEquals( expectedBed, matcher.getBedName(), "bed not matched" );
     }
 }
